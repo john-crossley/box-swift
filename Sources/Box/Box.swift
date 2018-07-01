5514ee98b2308a098fcb4abbdb92c0ce51eb2aef
John Crossley <jonnysnip3r@gmail.com>
2018-07-01 11:48:48 +0100

    Throw error if unable to load env file

--- Sources/Box/Box.swift
+++ Sources/Box/Box.swift
@@ -9,11 +9,15 @@ public final class Box {
     private var items = [String: String]()
 
     public init() {
-        let contents = try! loadContents()
+        do {
+            let contents = try loadContents()
 
-        for line in contents {
-            let groups = line.capturedGroups(regex: "([\\w\\_\\-]+)+(?:=)(.+)+")
-            items[groups[0]] = groups[1]
+            for line in contents {
+                let groups = line.capturedGroups(regex: "([\\w\\_\\-]+)+(?:=)(.+)+")
+                items[groups[0]] = groups[1]
+            }
+        } catch let error {
+            print("Unable to load environment file: \(error.localizedDescription)")
         }
     }
 
@@ -23,15 +27,12 @@ public final class Box {
     }
 
     private func loadContents() throws -> [String] {
-        let contents = try! String(contentsOf: URL(fileURLWithPath: envPath()))
+        let contents = try String(contentsOf: URL(fileURLWithPath: envPath()))
         return contents.components(separatedBy: .newlines).filter { !$0.isEmpty }
     }
 
     private func envPath() -> String {
-        if let path = ProcessInfo.processInfo.environment["ENV_FILE_PATH"] {
-            return path
-        }
-
-        return "/Users/jonno/dev/opensource/github/john-crossley/Box/.env"
+        return ProcessInfo.processInfo.environment["ENV_FILE_PATH"] ??
+            "/Users/jonno/dev/opensource/github/john-crossley/Box/.env"
     }
 }
